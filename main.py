67adf403fe5102d362d4baf0bba6bbce19db41f3
Vladimir Petrov <petrovvs@dev.vtb.ru>
2023-10-22 14:23:36 +0300

    Add query (2,3)

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import asyncio
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
 from fastapi.websockets import WebSocketState
 from fastapi.responses import FileResponse
-#import databases
-#import sqlalchemy
-#from databases import Database
 import uvicorn
 import pandas as pd
 from clickhouse_driver import Client
@@ -18,7 +15,6 @@ from fastapi.routing import APIRoute
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
-
 
 client = Client(host='75.119.142.124', port=9035, user='default', password='qolkasw10-=', database='default')
 
@@ -113,9 +109,7 @@ async def websocket_endpoint(websocket: WebSocket):
             new_data['event_timestamp'] = pd.to_datetime(new_data['event_timestamp'], format='%Y-%m-%d %H:%M:%S.%f')
             new_data['last_service_date'] = pd.to_datetime(new_data['last_service_date'], format='%Y-%m-%d %H:%M:%S.%f')
             new_data['last_service_date'].fillna(pd.Timestamp.min, inplace=True)
-
             df = pd.concat([df, new_data], ignore_index=True)
-
 
             if (time.time() - start) > 10:
                 start = time.time()
@@ -123,9 +117,11 @@ async def websocket_endpoint(websocket: WebSocket):
                 df_insert['id_forklift'] = df_insert['id_forklift'].astype(int)
                 df_insert['id_warehouse'] = df_insert['id_warehouse'].astype(int)
                 df_insert['id_task'] = df_insert['id_task'].astype(int)
-                file_csv.close()
-                client.execute('INSERT INTO main_data_stg_t2 (status, id_forklift, id_warehouse, id_task, id_point, event_timestamp,last_service_date) VALUES', list(df_insert.itertuples(index=False, name=None)))
-                selected_rows = df_insert[(df_insert['status'] == 'CHILL') | (df_insert['status'] == 'START')]
+                file_csv.close() # Сохраняем файл с логами, если при запросе к БД ошибка - данные не потеряются
+                selected_rows = df_insert[(df_insert['status'] == 'CHILL') | (df_insert['status'] == 'START')].copy()
+                df_insert = df_insert.drop('last_service_date', axis=1)
+                print(df_insert)
+                client.execute('INSERT INTO main_data_stg (status, id_forklift, id_warehouse, id_task, id_point, event_timestamp) VALUES', list(df_insert.itertuples(index=False, name=None)))
                 selected_columns = selected_rows[['id_forklift', 'id_warehouse', 'last_service_date']]
                 selected_columns = selected_columns.rename(columns={'last_service_date': 'last_maintenance_date','id_warehouse':'id_warehousr'})
                 selected_columns['next_maintenance_date'] = selected_columns['last_maintenance_date'] + pd.Timedelta(days=181)
@@ -139,6 +135,15 @@ async def websocket_endpoint(websocket: WebSocket):
         print('Error----------------------------------------------',ex)
         file_csv.close()
 
+
+
+class LoaderInfo(BaseModel):
+    id_forklift: int
+    status: str
+    id_task: int
+    last_maintenance_date: datetime
+    next_maintenance_date: datetime
+
 def list_routes():
     routes = []
     for route in app.routes:
@@ -150,14 +155,159 @@ def list_routes():
 async def home(request: Request):
     routes = list_routes()
     return templates.TemplateResponse("routes_template.html", {"request":request, "routes": routes})
-    
 
-class LoaderInfo(BaseModel):
-    id_forklift: int
-    status: str
-    id_task: int
-    last_maintenance_date: datetime
-    next_maintenance_date: datetime
+
+@app.get("/query1/{id_forklift}/{id_warehouse}/{from_ts}/{to_ts}")   # YYYY-MM-DD
+async def get_loader_info(id_forklift: int, id_warehouse: int,from_ts,to_ts):
+    dists = {'K1': {'K1': 0, 'K2': 5},
+    'K2': {'K2': 0, 'K1': 5, 'K3': 10, 'K5': 5},
+    'K3': {'K3': 0, 'K2': 10, 'X1': 10, 'K4': 15},
+    'X1': {'X1': 0, 'K3': 10},
+    'K4': {'K4': 0, 'K3': 15, 'X2': 10},
+    'X2': {'X2': 0, 'K4': 10},
+    'K5': {'K5': 0, 'K2': 5, 'K6': 10, 'K8': 5},
+    'K6': {'K6': 0, 'K5': 10, 'X3': 10, 'K7': 15},
+    'X3': {'X3': 0, 'K6': 10},
+    'K7': {'K7': 0, 'K6': 15, 'X4': 10},
+    'X4': {'X4': 0, 'K7': 10},
+    'K8': {'K8': 0, 'K5': 5, 'K9': 10},
+    'K9': {'K9': 0, 'K8': 10, 'X5': 5, 'K10': 15},
+    'X5': {'X5': 0, 'K9': 5},
+    'K10': {'K10': 0, 'K9': 15, 'X6': 10},
+    'X6': {'X6': 0, 'K10': 10}}
+    # Выполните SQL-запрос к ClickHouse для извлечения информации
+    from_datetime = datetime.strptime(from_ts, "%Y-%m-%d")
+    to_datetime = datetime.strptime(to_ts, "%Y-%m-%d")
+    print(from_datetime,to_datetime)
+    query = f"""
+SELECT *
+FROM `default`.main_data_stg
+WHERE id_forklift = {id_forklift}
+and id_warehouse =  {id_warehouse}
+and event_timestamp BETWEEN '{from_datetime}' and '{to_datetime}'
+ORDER by event_timestamp ASC ;
+                """
+    print(query)
+    
+    try:
+        result = client.execute(query)
+        if result ==[]:
+            return {}
+        df = pd.DataFrame(result, columns=[desc[0] for desc in client.execute("DESCRIBE `default`.main_data_stg")])
+        df['dist'] = df.apply(lambda row: dists[row['id_point']][row['id_next_point']],axis=1)
+        result = df['dist'].sum()
+        print(result)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    if not result:
+        raise HTTPException(status_code=404, detail="Loader information not found")
+
+
+    return result
+
+
+@app.get("/query2/{id_forklift}/{id_warehouse}/{from_ts}/{to_ts}")   # YYYY-MM-DD
+async def get_loader_info(id_forklift: int,id_warehouse: int,from_ts,to_ts):
+    # Выполните SQL-запрос к ClickHouse для извлечения информации
+    from_datetime = datetime.strptime(from_ts, "%Y-%m-%d")
+    to_datetime = datetime.strptime(to_ts, "%Y-%m-%d")
+    print(from_datetime,to_datetime)
+    query = f"""
+SELECT COUNT(*)
+FROM `default`.main_data_stg
+WHERE id_forklift = id_forklift 
+and id_warehouse =  id_warehouse
+and event_timestamp BETWEEN '{from_datetime}' and '{to_datetime}'
+and status = 'FINISH'
+                """
+    print(query)
+    
+    try:
+        result = client.execute(query)
+        if result ==[]:
+            return {}
+        print(result)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    if not result:
+        raise HTTPException(status_code=404, detail="Loader information not found")
+    return result
+
+
+@app.get("/query3/{id_forklift}/{id_warehouse}/{from_ts}/{to_ts}/{num}")   # YYYY-MM-DD
+async def get_loader_info(id_forklift: int,id_warehouse: int,from_ts,to_ts,num: int):
+    # Выполните SQL-запрос к ClickHouse для извлечения информации
+    from_datetime = datetime.strptime(from_ts, "%Y-%m-%d")
+    to_datetime = datetime.strptime(to_ts, "%Y-%m-%d")
+    query = f"""
+SELECT *
+FROM `default`.main_data_stg
+WHERE id_forklift = id_forklift 
+and id_warehouse =  id_warehouse
+and event_timestamp BETWEEN '{from_datetime}' and '{to_datetime}'
+AND (status IN ('FINISH','START')) 
+ORDER by event_timestamp ASC ;
+
+                """
+    
+    try:
+        result = client.execute(query)
+        if result ==[]:
+            return {}
+        df = pd.DataFrame(result, columns=[desc[0] for desc in client.execute("DESCRIBE `default`.main_data_stg")])
+        # leave only start/finish events. assume the rover moves all the time, while not on k1 station
+        idle_time = (df['event_timestamp'] - df['event_timestamp'].shift(1)).sum() 
+        if df['status'][0] == 'START':
+            idle_time = (df['event_timestamp'] - df['event_timestamp'].shift(1))[1::2].sum()
+        else:
+            idle_time (df['event_timestamp'] - df['event_timestamp'].shift(1))[::2].sum()
+        busy_time = df['event_timestamp'].max() - df['event_timestamp'].min() - idle_time
+        if num==1:
+            return busy_time   
+        else:
+            return idle_time
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    if not result:
+        raise HTTPException(status_code=404, detail="Loader information not found")
+    return result
+
+@app.get("/query4/{id_forklift}/{id_warehouse}/{from_ts}/{to_ts}")   # YYYY-MM-DD
+async def get_loader_info(id_forklift: int,id_warehouse: int,from_ts,to_ts):
+    # Выполните SQL-запрос к ClickHouse для извлечения информации
+    from_datetime = datetime.strptime(from_ts, "%Y-%m-%d")
+    to_datetime = datetime.strptime(to_ts, "%Y-%m-%d")
+    print(from_datetime,to_datetime)
+    query = f"""
+SELECT *
+FROM `default`.main_data_stg
+WHERE id_forklift = id_forklift 
+and id_warehouse =  id_warehouse
+and event_timestamp BETWEEN '{from_datetime}' and '{to_datetime}'
+ORDER by event_timestamp ASC ;
+
+                """
+    print(query)
+    
+    try:
+        result = client.execute(query)
+        if result ==[]:
+            return {}
+        df = pd.DataFrame(result, columns=[desc[0] for desc in client.execute("DESCRIBE `default`.main_data_stg")])
+        resps = df[df['status'] != df['status'].shift()]
+       # print(resps)
+        resps['time_in_state'] = resps['event_timestamp'] - df['event_timestamp'].shift()
+        result = resps.groupby(['status', 'id_forklift', 'id_warehouse']).aggregate({'time_in_state': 'sum' })
+        print(result)
+        # Преобразовать результат агрегации в JSON
+        result = result.to_json(orient='split')
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+   # if not result:
+    #    raise HTTPException(status_code=404, detail="Loader information not found")
+
+    return result
 
 
 @app.get("/forklift_info/{id_forklift}/{id_warehouse}")
